f9c7b3458fc6d4f6b263d413e3fa0c76ae1bc36a
Daniel Hines <d4hines@gmail.com>
2023-06-08 15:33:39 -0400

    remove dead templates

--- flake.nix
+++ flake.nix
@@ -7,18 +7,8 @@
         path = ./ocaml;
         description = "OCaml development flake";
       };
-
-      hello-ocaml = {
-        path = ./hello-ocaml;
-        description = "A first look at Nix flakes";
-      };
-
-      hello-ocaml-dream = {
-        path = ./hello-ocaml-dream;
-        description = "An extension of the hello-ocaml flake into a more full environment";
-      };
-
-      node-typescript-simple = {
+      
+      node-typescript = {
         path = ./node-typescript-simple;
         description = "A NodeJS/TypeScript development flake";
       };
